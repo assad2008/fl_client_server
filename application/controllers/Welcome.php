1bb42a2346773a9594bd9c8c069d689e8843a2b2
上官江 <rlk002@gmail.com>
2014-03-21 22:20:16 +0800

    fix welcome
    add

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,13 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends My_Controller
 {
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function index()
-	{
-		$this->echodata($this->recvdata);
-	}
+    public function test()
+    {
+        $this->echodata(1,array('List' => array('Do' => 'This is a test')));
+    }
 }
+
--- data/rplogs/rplog20140319.php
+++
@@ -1,2 +0,0 @@
-<?die;?> 2014-03-19 13:46:55 - movie/start - {"null":"null"} -  - null
-<?die;?> 2014-03-19 13:48:53 - movie/start - {"null":"null"} -  - null
